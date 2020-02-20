06df1770acf045a44281bc53716821c4416faa4f
Daniel Vargas <daniel.vargas@sga.pucminas.br>
2020-02-20 02:49:41 -0300

    Modificações para obter 100 repositórios

--- GitLab.py
+++ GitLab.py
@@ -1,51 +1,61 @@
 import requests
+import time
 
-headers = {"Authorization": "token a2899f94ada7b3da849e42ad0a1980f6985cbd9a"}
+headers = {"Authorization": "token 9447a02281c83005f5d9858dd675b86cbb5aa340"}
 
+initial = "null"
 
 def run_query(query):
     request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+    while (request.status_code == 502):
+          time.sleep(2)
+          request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
     if request.status_code == 200:
         return request.json()
     else:
         raise Exception("Query falhou! Codigo de retorno: {}. {}".format(request.status_code, query))
 
-               
-query = """
-{
-  search(query:"stars:>100", type:REPOSITORY, first:100){
-    nodes{
-      ... on Repository
+for x in range(5):        
+      query = """
       {
-        nameWithOwner
-        url
-        primaryLanguage
-        {
-          name
+        search(query:"stars:>100", type:REPOSITORY, first:20, after:%s){
+          nodes{
+            ... on Repository
+            {
+              nameWithOwner
+              url
+              primaryLanguage
+              {
+                name
+              }
+              acceptedPullRequests: pullRequests(states:MERGED)
+              {
+                totalCount
+              }
+              releases
+              {
+                totalCount
+              }
+              closedIssues: issues(states:CLOSED)
+              {
+                totalCount
+              }
+              totalIssues: issues
+              {
+                totalCount
+              }
+              createdAt
+              updatedAt
+            }
+          }
+          pageInfo
+          {
+            endCursor
+          }
         }
-        acceptedPullRequests: pullRequests(states:MERGED)
-        {
-          totalCount
-        }
-        releases
-        {
-          totalCount
-        }
-        closedIssues: issues(states:CLOSED)
-        {
-          totalCount
-        }
-        totalIssues: issues
-        {
-          totalCount
-        }
-        createdAt
-        updatedAt
       }
-    }
-  }
-}
-"""
+      """ % (initial)
 
-result = run_query(query)
-print(result)
+      result = run_query(query)
+      print(result)
+      initial = '"{}"'.format(result["data"]["search"]["pageInfo"]["endCursor"])
